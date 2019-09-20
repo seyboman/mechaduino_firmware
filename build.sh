6d8e34ae3c948038e55ad2260f193b738d2d61b8
Florian Seybold <florian@seybold.space>
2019-09-20 10:47:35 +0200

    relative path

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make BOARD=mechaduino LINKER_SCRIPT=/home/seyboman/ros2_riot_ws/install/RIOT/cpu/sam0_common/ldscripts/samd21j18a_arduino_bootloader.ld PORT=tap0
+make BOARD=mechaduino LINKER_SCRIPT=../RIOT/cpu/sam0_common/ldscripts/samd21j18a_arduino_bootloader.ld PORT=tap0
